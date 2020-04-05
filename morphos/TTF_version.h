77f900bab1b1d5c80e80a7ba93325c8750836879
BSzili <col.lawrence@gmail.com>
2020-04-05 16:46:35 +0200

    MorphOS port

---
+++ morphos/TTF_version.h
@@ -0,0 +1,6 @@
+#define	VERSION	52
+#define	REVISION	0
+#define	DATE	"04.04.2020"
+#define	VERS	"sdl2_ttf.library 52.0"
+#define	VSTRING	"sdl2_ttf.library 52.0 (04.04.2020) © Ilkka Lehtoranta, Szilard Biro\r\n"
+#define	VERSTAG	"\0$VER: sdl2_ttf.library 52.0 (04.04.2020) © Ilkka Lehtoranta, Szilard Biro"
