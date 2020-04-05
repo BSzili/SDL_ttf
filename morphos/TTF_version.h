f3755cc32ae6bf02e81e5278be59604a6328e030
BSzili <col.lawrence@gmail.com>
2020-04-05 18:01:34 +0200

    bumped the library version to 53

--- morphos/TTF_version.h
+++ morphos/TTF_version.h
@@ -1,6 +1,6 @@
-#define	VERSION	52
+#define	VERSION	53
 #define	REVISION	0
 #define	DATE	"04.04.2020"
-#define	VERS	"sdl2_ttf.library 52.0"
-#define	VSTRING	"sdl2_ttf.library 52.0 (04.04.2020) © Ilkka Lehtoranta, Szilard Biro\r\n"
-#define	VERSTAG	"\0$VER: sdl2_ttf.library 52.0 (04.04.2020) © Ilkka Lehtoranta, Szilard Biro"
+#define	VERS	"sdl2_ttf.library 53.0"
+#define	VSTRING	"sdl2_ttf.library 53.0 (04.04.2020) © Ilkka Lehtoranta, Szilard Biro\r\n"
+#define	VERSTAG	"\0$VER: sdl2_ttf.library 53.0 (04.04.2020) © Ilkka Lehtoranta, Szilard Biro"
